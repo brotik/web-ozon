a13b50d81ded01e1097757e1d114293c4a95cd15
admin <brotik13@gmail>
2019-03-29 20:27:54 +0300

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
+import os
+
+import waitress
 from flask import render_template, Flask, request, url_for, redirect
 
 from app.ozon import create_book, add_book, search_book_by_title, search_book_by_id, remove_book_by_id, create_empty_book
 
 
-def main():
+def start():
     app = Flask(__name__)
 
     container = []
@@ -55,8 +58,11 @@ def main():
         container = remove_book_by_id(container, book_id)
         return redirect(url_for('index'))
 
-    app.run(port=9876, debug=True)
+    if os.getenv('APP_ENV') == 'PROD' and os.getenv('PORT'):
+        waitress.serve(app, port=os.getenv('PORT'))
+    else:
+        app.run(port=9876, debug=True)
 
 
 if __name__ == '__main__':
-    main()
+    start()
